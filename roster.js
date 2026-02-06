081246da95b0c2b43b9f63c7d688fba675e4a4f9
Francis Zheng <francisdzheng@gmail.com>
2026-02-06 15:49:03 +0900

    update roster

--- roster.js
+++ roster.js
@@ -61,10 +61,36 @@ const ROSTER = [
 // Map: Slack User ID → Roster Name
 // Replace 'UXXXXXXXXXX' with actual Slack user IDs
 const USER_ID_TO_NAME = new Map([
-  // ['U01ABC1234', 'Miho'],
-  // ['U01ABC1235', 'fortkle'],
-  // ['U01ABC1236', 'Rory'],
-  // ... add all team members
+  ['U0A4FL5QD29', 'Francis'],
+  ['U090X3J58KT', 'Miho'],
+  ['U046E19K6E7', 'fortkle'],
+  ['U077P1CKQ73', 'Rory'],
+  ['U04ERHD8R2P', 'chikitam'],
+  ['U05QM3HTNDT', 'mio'],
+  ['U08MNDQSPQ8', 'yuki'],
+  ['U04RESHBGH0', 'Neko'],
+  ['U01AYBXHPLP', 'Ramen'],
+  ['U072NBJEDK3', 'Akihiro'],
+  ['U0ABPSEHV9D', 'Takeo'],
+  ['U06D1LRS5B4', 'Kei'],
+  ['U09L5FE1C1G', 'Shabliss'],
+  ['U069HCGMZ8S', 'Tai'],
+  ['U096ERTUWF4', 'Rio'],
+  ['U079ESLHRU0', 'Tao'],
+  ['U08AB2961ND', 'kimuson'],
+  ['U05LB7P0WQ2', 'Syori'],
+  ['U0857G8MN8K', 'chui'],
+  ['U0624E5TQQM', 'Yume'],
+  ['U07K4ESCT6C', 'Rola'],
+  ['U0A3XCT5M28', 'Pon'],
+  ['U06UXSEC404', 'Apippo'],
+  ['U01GA8KR47L', 'ESD'],
+  ['U079G4KCBDH', 'Kyle'],
+  ['U09G7D2I3K3', 'Kuroki'],
+  ['U07CXVAVCBZ', 'Kanata'],
+  ['U093BMT9P0A', 'Mohnya'],
+  ['U07LLQWSZHS', 'Davide'],
+  ['U01BA1H9S5B', 'Cap'],
 ]);
 
 module.exports = { ROSTER, USER_ID_TO_NAME };
